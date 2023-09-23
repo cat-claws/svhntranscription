ffd8b33e1b7c1183707b040ca07227d45dba704b
cat-claws <47313357+cat-claws@users.noreply.github.com>
2023-09-23 16:55:18 +0800

    train classifiers

--- model_d.py
+++ model_d.py
@@ -2,23 +2,6 @@ import torchvision
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
 
-# backbone = torchvision.models.mobilenet_v2(weights="DEFAULT").features
-# backbone.out_channels = 1280
-
-
-# anchor_generator = AnchorGenerator(sizes=((8, 16, 32, 64, 96),),
-#                                    aspect_ratios=((0.5, 1.0, 2.0),))
-
-# roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
-#                                                 output_size=7,
-#                                                 sampling_ratio=2)
-# def detector():
-#     return FasterRCNN(backbone,
-#                    num_classes=2,
-#                    rpn_anchor_generator=anchor_generator,
-#                    box_roi_pool=roi_pooler)
-
-
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(weights='DEFAULT')
 # model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
